5a406624bdb9a7c21b7284da270bc4f41ac597c3
qiaoshouzi <1957477224@qq.com>
2021-02-27 00:57:47 +0800

    更新确保logs文件能被识别

--- start.py
+++ start.py
@@ -57,7 +57,7 @@ logging.basicConfig(level = logging.INFO,format = '%(lineno)d | %(asctime)s - %(
   #创建
 logger = logging.getLogger("AutoRecorded")
   #创建handler
-handler1 = logging.FileHandler("log\\log-"+str(int(time.time()))+".log") #设置log文件名格式
+handler1 = logging.FileHandler("logs\\log-"+str(int(time.time()))+".log") #设置log文件名格式
 handler1.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s|%(name)-12s+ %(levelname)-8s++%(message)s') #设置 log文件 显示log的样式
 handler1.setFormatter(formatter)
